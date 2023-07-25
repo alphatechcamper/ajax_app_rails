b48571db7d978469b5a36aded0c458d93449f410
alphatechcamper <mizumoto2017@gmail.com>
2023-07-25 21:13:06 +0900

    sa

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -21,7 +21,7 @@ function post (){
     XHR.open("POST", "/posts", true);  //「posts」→「post」にする
     XHR.responseType = "json";
     XHR.send(formData);
-    XHR.onload = () => {
+    XHR.onload = () => { 
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
